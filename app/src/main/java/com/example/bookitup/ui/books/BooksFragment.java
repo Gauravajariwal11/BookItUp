777b6dc77c44f53d31721058f1fc3a7c9cb7444e
An Nguyen <an.nguyen6@mavs.uta.edu>
2020-03-21 14:32:10 -0500

    fix icons, add My books side bar menu

--- app/src/main/java/com/example/bookitup/ui/books/BooksFragment.java
+++ app/src/main/java/com/example/bookitup/ui/books/BooksFragment.java
@@ -1,4 +1,4 @@
-package com.example.bookitup.ui.gallery;
+package com.example.bookitup.ui.books;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.bookitup.R;
 
-public class GalleryFragment extends Fragment {
+public class BooksFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private BooksViewModel booksViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        booksViewModel =
+                ViewModelProviders.of(this).get(BooksViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_books, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        booksViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/bookitup/ui/books/BooksViewModel.java
+++ app/src/main/java/com/example/bookitup/ui/books/BooksViewModel.java
@@ -1,14 +1,14 @@
-package com.example.bookitup.ui.gallery;
+package com.example.bookitup.ui.books;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class BooksViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public BooksViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
